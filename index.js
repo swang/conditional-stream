8b388e6faff1208b7aed45368ae2efb91f166800
Shuan Wang <shuanwang@gmail.com>
2015-03-22 02:02:48 -0700

    Converting this library to ES6
    In an effort to test out ES6, I've converted this small library to use
    ES6.

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
 'use strict';
 
-var duplex = require('stream').Duplex
-  , inherits = require('util').inherits
-
-function ConditionalStream(condFunc, opts) {
-  if (!(this instanceof ConditionalStream)) {
-    return new ConditionalStream(condFunc, opts);
-  }
-
-  duplex.call(this, opts)
-
-  if (typeof condFunc !== 'function') {
-    throw new Error('Must pass a function as first parameter')
-  }
-  this.condFunc = condFunc
-}
-
-inherits(ConditionalStream, duplex)
-
-ConditionalStream.prototype._read = function() {};
-
-ConditionalStream.prototype._write = function(chunk, enc, callback) {
-  if (chunk === null) {
-    return this.push('')
-  }
-  if (this.condFunc(chunk)) {
-    this.push(chunk)
-  }
-  callback()
-}
-
-module.exports = ConditionalStream
+module.exports = require('./lib/index.js')
---
+++ src/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var { Duplex: duplex } = require('stream')
+
+export default class ConditionalStream extends duplex {
+  constructor(condFunc, opts) {
+    if (!(this instanceof ConditionalStream)) {
+      return new ConditionalStream(condFunc, opts);
+    }
+
+    super(opts)
+
+    if (typeof condFunc !== 'function') {
+      throw new Error('Must pass a function as first parameter')
+    }
+
+    this.condFunc = condFunc
+  }
+  _read() {}
+  _write(chunk, enc, callback) {
+    if (chunk === null) {
+      return this.push('')
+    }
+    if (this.condFunc(chunk)) {
+      this.push(chunk)
+    }
+    callback()
+  }
+}
